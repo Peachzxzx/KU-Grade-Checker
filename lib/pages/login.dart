bf559196b31a3e297b9d680f2c5cfeb4ee256c10
025528078pP <peachzxzx@hotmail.com>
2020-11-28 21:53:38 +0700

    Create login page

---
+++ lib/pages/login.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _usernameFilter = new TextEditingController();
+  final TextEditingController _passwordFilter = new TextEditingController();
+  String _username = "";
+  String _password = "";
+
+  _LoginPageState() {
+    _usernameFilter.addListener(_usernameListen);
+    _passwordFilter.addListener(_passwordListen);
+  }
+
+  void _usernameListen() {
+    if (_usernameFilter.text.isEmpty) {
+      _username = "";
+    } else {
+      _username = _usernameFilter.text;
+    }
+  }
+
+  void _passwordListen() {
+    if (_passwordFilter.text.isEmpty) {
+      _password = "";
+    } else {
+      _password = _passwordFilter.text;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: _buildBar(context),
+      body: new Container(
+        padding: EdgeInsets.all(16.0),
+        child: new Column(
+          children: <Widget>[
+            _buildTextFields(),
+            _buildButtons(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBar(BuildContext context) {
+    return new AppBar(
+      title: new Text("KU Auto Grade Check"),
+    );
+  }
+
+  Widget _buildTextFields() {
+    final node = FocusScope.of(context);
+    return new Container(
+      child: new Column(
+        children: <Widget>[
+          new Container(
+            child: new TextField(
+                controller: _usernameFilter,
+                decoration: new InputDecoration(labelText: 'Nontri Account'),
+                onEditingComplete: () => node.nextFocus()),
+          ),
+          new Container(
+            child: new TextField(
+                controller: _passwordFilter,
+                decoration: new InputDecoration(labelText: 'Password'),
+                obscureText: true,
+                onSubmitted: (_) => node.unfocus()),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildButtons() {
+    return new Container(
+      child: new Column(
+        children: <Widget>[
+          new RaisedButton(
+              child: new Text(
+                'Login',
+                style: TextStyle(color: Color(0xFFFFFFFF)),
+              ),
+              onPressed: _loginPressed,
+              color: Color(0xFFB2BB1E)),
+        ],
+      ),
+    );
+  }
+
+  // These functions can self contain any user auth logic required, they all have access to _username and _password
+
+  void _loginPressed() {
+    Navigator.pushNamed(context, '/info');
+  }
+}
